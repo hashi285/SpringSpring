0ad8e78d386213907a7eb9e7004c13536c46ccdc
hashi285 <m51187251@gmail.com>
2024-05-21 23:52:08 +0900

    안녕~

--- src/main/java/org/example/springpre/컨트롤러/FormController.java
+++ src/main/java/org/example/springpre/컨트롤러/FormController.java
@@ -1,7 +1,9 @@
 package org.example.springpre.컨트롤러;
 
 
+import org.example.springpre.도메인.UserForm;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,7 +11,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class FormController {
     @GetMapping("/form")
-    public String showForm(){
+    public String showForm(Model model){
+        model.addAttribute("userForm", new UserForm());
         return "form";
     }
 
--- src/main/java/org/example/springpre/컨트롤러/ListController.java
+++ src/main/java/org/example/springpre/컨트롤러/ListController.java
@@ -1,10 +1,19 @@
 package org.example.springpre.컨트롤러;
 
-import ch.qos.logback.core.model.Model;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+@Controller
+public class ListController {
+    @GetMapping("/list")
+    public String showList(Model model) {
+        List<String> items = Arrays.asList("Item1","Item2", "Item3", "Item4");
+        model.addAttribute("items", items);
+        return "list";
+    }
+}
